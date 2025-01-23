b5b0aa3173157e4f48b65d33b2cf1b21ae37059e
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-23 12:51:24 +0600

    start to commit

---
+++ 01.Basics/app.js
@@ -0,0 +1,16 @@
+// console.table({ name: "jack", age: 21 });
+
+// console.dir({ name: "Jack", age: 21 });
+
+var a = document.querySelector("h1").textContent;
+
+let b = 1;
+
+const c = 30;
+
+let myName = "jackSparrow"
+
+
+console.log(typeof (b))
+
+console.log(myName)
