69227c828d5bfd5117cd64c1dc072a9986f18d90
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-23 12:54:20 +0600

    23_Jan_25|12:54PM

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -3,14 +3,13 @@
 // console.dir({ name: "Jack", age: 21 });
 
 var a = document.querySelector("h1").textContent;
-
 let b = 1;
-
 const c = 30;
+let myName = "jackSparrow";
+const h1 = document.querySelector("h1");
 
-let myName = "jackSparrow"
+console.log(h1);
 
+console.log(typeof b);
 
-console.log(typeof (b))
-
-console.log(myName)
+console.log(myName);
