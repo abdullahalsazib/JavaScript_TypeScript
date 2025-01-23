71b2c567c7165e3d35745fa479d9584a2ff367b1
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-23 14:04:08 +0600

    23_Jan_25|2:04PM

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -13,3 +13,109 @@ console.log(h1);
 console.log(typeof b);
 
 console.log(myName);
+
+// Basic Math
+
+/*
+console.log(2 + 2);
+console.log(2 - 3);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+*/
+
+// counter
+let counter = 1;
+counter++;
+
+console.log(counter);
+
+// Boolean
+
+let isTrue = true;
+console.log(isTrue);
+
+// undefiend
+let number = 10;
+
+console.log(number + undefined); // NaN --> Not a Number
+
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let fevNub = 21;
+
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(fevNub);
+
+// Comparison Operators
+// --> Relational Operator
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+// --> Equality Operator
+//  === strict equality ( type + value )
+//  !== strict non-equality ( type + value )
+//  == lose equality operator ( value )
+//  != lose not equality operator ( value )
+
+console.log(10 === 10);
+console.log(10 === "10");
+console.log(10 !== "10");
+console.log(10 === Number("10"));
+console.log(10 == 10);
+console.log(10 != 10);
+
+let firstName = "Jack";
+let secondName = "Sparrow";
+let fullName = "";
+// append
+
+// console.log(firstName + secondName);
+
+fullName += firstName + secondName;
+
+console.log(fullName);
+
+// Length
+console.log(firstName.length);
+console.log(secondName.length);
+console.log(fullName.length);
+
+// Cases
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.toLocaleUpperCase());
+
+// Slice
+console.log(firstName.slice(0, 1).toUpperCase());
+
+// Split & Join
+console.log(firstName.split("").join(" _ ").toUpperCase());
+
+// Includes
+console.log(firstName.includes("W"));
+
+// Trim
+console.log(firstName.trim());
+
+let FullName = `${firstName} and ${secondName}`;
+console.log(FullName);
+
+console.log("jack");
+
+// Type conversion
+// one type to another type
+
+let money = "50";
+// money = parseInt(money)
+// money = +money
+money = Number(money);
+console.log(money);
+
+//  conver number to string
+
+money = money.toString();
+console.log(typeof money);
