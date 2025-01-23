843de5942a67e55645226bd264edc96969213b09
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-23 20:53:21 +0600

    23_Jan_25|8:53PM

--- TypeScript/01.Module/src/script.ts
+++ TypeScript/01.Module/src/script.ts
@@ -1,4 +1,148 @@
-console.log("hello, world");
+console.log("hello, world that is from TypeScript to JavaScript");
 
-const country = "Bangladesh";
-console.log(country);
+// const country = "Bangladesh";
+// console.log(country);
+
+// let playerName = "jack sparrow";
+
+// playerName = "abdullah al sazib";
+
+// console.log(playerName);
+
+// function
+// function myFunction() {
+//   console.log("this console is from myFunction");
+// }
+// myFunction();
+
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+// console.log(multiply(10, 20));
+
+// array
+// let fruits = ["apple", "banana", "orange", 30, false, {}];
+// fruits.push(300);
+// fruits.push(false);
+// fruits.push({
+//   name: "jack sparrow",
+// });
+// console.log(fruits);
+
+// object
+// let person = {
+//   name: "jack sparrow",
+//   age: 32,
+//   isCaptain: true,
+// };
+// person.age = 21;
+// console.log(person.age);
+
+// let a: string;
+// let b: number;
+
+// a = "jack";
+// b = 21;
+// console.log(a);
+// console.log(b);
+
+// array
+
+// let arr: string[] = [];
+// arr.push("jack");
+
+// console.log(arr);
+
+// array
+// let arr1: (string | number | boolean)[] = [];
+// let arr: any[] = [];
+// arr.push(10);
+// arr.push("Jack");
+// arr.push(true);
+
+// console.log(arr);
+// console.log(arr1);
+
+// object
+
+// let c: {
+//   name: string;
+//   age: number;
+//   isAdult: boolean;
+//   car: Function;
+// };
+
+// /*
+// c = {
+//   name: "jack",
+//   age: 21,
+//   isAdult: true,
+//   car: function (carname: string) {
+//     console.log(`not have car ${carname.toUpperCase()}`);
+//   },
+// };
+// */
+// const me = new Object(
+//   (c = {
+//     name: "jack",
+//     age: 21,
+//     isAdult: true,
+//     car: function (carName: string) {
+//       if (carName === undefined || carName === "") {
+//         carName = "Guest Car";
+//         console.log(`${this.name} car name is ${carName}`);
+//       } else {
+//         console.log(`${this.name} car name is ${carName}`);
+//       }
+//     },
+//   })
+// );
+
+// c.car("");
+
+// function
+
+// const myFunc = () => {
+//   console.log("hello");
+// };
+
+// const myFunc: Function = (
+//   name: string,
+//   age: number,
+//   isAdult: boolean = false
+// ) => {
+//   console.log(name);
+//   console.log(age);
+//   console.log(isAdult);
+// };
+
+// myFunc("jack", 32);
+
+/*
+class Person {
+  name: string;
+  age: number;
+  isAdult: boolean;
+
+  constructor(
+    name: string = "Unknown",
+    age: number = 0,
+    isAdult: boolean = false
+  ) {
+    this.name = name;
+    this.age = age;
+    this.isAdult = isAdult;
+  }
+
+  // method
+  geeter(): string {
+    return `Hello, my name is ${this.name} and my age is ${this.age} and i'm a ${this.isAdult}`;
+  }
+}
+
+const p1 = new Person();
+
+const a = p1.geeter();
+
+console.log(a);
+*/
