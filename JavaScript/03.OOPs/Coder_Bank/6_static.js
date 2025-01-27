addf572cef72fd0f6b6bfa0173f330956ca71cc8
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-27 18:58:36 +0600

    27_Jan_25|6:54PM

--- JavaScript/03.OOPs/Coder_Bank/6_static.js
+++ JavaScript/03.OOPs/Coder_Bank/6_static.js
@@ -1,25 +1,36 @@
-class User {
-  constructor(name, age, income) {
-    this.name = name;
-    this.age = age;
-    this.income = income;
-  }
-
-  static sortByAge(user1, user2) {
-    return user1.age - user2.age;
-  }
-  static sortByIncome(user1, user2) {
-    return user1.age - user2.age;
-  }
+class config {
+  static dbName = "dbName";
+  static dbpass = "dbPass";
+  static apiToken = "abcd";
 }
 
-const user1 = new User("Jack", 21, 20000);
-const user2 = new User("John", 71, 2000);
-const user3 = new User("sazib", 19, 5000);
+console.log(config.apiToken);
 
-const arrUser = [user1, user2, user3];
+// // let id = 1;
+// class User {
+//   static id = 1;
+//   constructor(name, age, income) {
+//     this.name = name;
+//     this.age = age;
+//     this.income = income;
+//     this.id = User.id;
+//   }
 
-console.log(arrUser);
-arrUser.sort(User.sortByIncome);
+//   // static sortByAge(user1, user2) {
+//   //   return user1.age - user2.age;
+//   // }
+//   // static sortByIncome(user1, user2) {
+//   //   return user1.age - user2.age;
+//   // }
+// }
 
-console.log(arrUser);
+// const user1 = new User("Jack", 21, 20000, id);
+// const user2 = new User("John", 71, 2000, id);
+// const user3 = new User("sazib", 19, 5000, id);
+
+// const arrUser = [user1, user2, user3];
+
+// console.log(arrUser);
+// arrUser.sort(User.sortByIncome);
+
+// console.log(arrUser);
