456c030af22bba1b61069595171f8e1bb0ea1f20
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-28 15:59:44 +0600

    28_Jan_25|3:59PM

--- JavaScript/03_Async/Promise.js
+++ JavaScript/03_Async/Promise.js
@@ -1,42 +1,53 @@
-
 const paymentSuccess = true;
-const mark = 70;
+const mark = 90;
 
 // Enroll
-let enroll = () =>{
-  console.log("Course enrollment is in progress...");
+let enroll = () => {
+  console.log("Course enrollment is in progress...🚀");
 
-  const promise = new Promise((resolv, reject)=> {
-    setTimeout(()=> {
-      if(paymentSuccess){
+  const promise = new Promise((resolv, reject) => {
+    setTimeout(() => {
+      if (paymentSuccess) {
         resolv();
-      }else {
-        reject("Payment Faild..!")
+      } else {
+        reject("Payment Faild..!");
       }
-    }, 2000)
-  })
+    }, 2000);
+  });
 
   return promise;
-}
+};
 
 // Progress
 let progress = () => {
-  console.log("Course on Progress......")
-  const promise = new Promise((resolv, reject)=> {
-     setTimeout(() => {
-    if(mark >= 80){
-      resolv()
-    }else {
-      reject("You could not get enough marks to get the certificate")
-    }
-  },2000)
-  })
+  console.log("Course on Progress......");
 
-return promise;
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (mark >= 80) {
+        resolve();
+      } else {
+        reject("You could not get enough marks to get the certificate");
+      }
+    }, 2000);
+  });
+};
+let getCertificate = () => {
+  console.log("Prearing your certificate");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Congrets! you got the certificate 😄");
+    }, 1000);
+  });
+};
 
-
-
+enroll()
+  .then(progress)
+  .then(getCertificate)
+  .then((value) => console.log(value))
+  .catch((err) => {
+    console.log(err);
+  });
 
 /*
 const status = true;
