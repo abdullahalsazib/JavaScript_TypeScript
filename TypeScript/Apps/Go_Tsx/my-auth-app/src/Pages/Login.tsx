006395c95b2b497b586e5b101cadaa88b0e8d16b
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-02-07 10:53:28 +0600

    7_Feb_25|10:53PM

--- TypeScript/Apps/Go_Tsx/my-auth-app/src/Pages/Login.tsx
+++ TypeScript/Apps/Go_Tsx/my-auth-app/src/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { getUser, login } from "../api/Auth";
@@ -11,6 +11,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
+    console.log("hello world");
     try {
       await login({ email, password });
       const userData = await getUser();
@@ -25,23 +26,62 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className=" w-full h-screen flex items-center justify-center flex-col">
+      <form
+        onSubmit={handleLogin}
+        className="flex items-center justify-center gap-5 bg-slate-50 flex-col p-5 rounded-md shadow-2xl"
+      >
+        <h2 className="text-3xl text-center font-bold uppercase font-mono">
+          Login form
+        </h2>
+        <InputFild placeholder="Enter your email.." type="email" />
+        <InputFild placeholder="Enter your password.." type="password" />
+        {/* <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /> */}
+        <MyButton title="Log In" type="submit" />
+      </form>
     </div>
   );
 };
 
 export default Login;
+
+interface InputFildProps {
+  type: string;
+  placeholder: string;
+}
+const InputFild: React.FC<InputFildProps> = (props) => {
+  return (
+    <>
+      <input
+        className=" border-2 border-gray-300 hover:border-gray-400 duration-200 rounded-md py-2 px-4 outline-none"
+        type={props.type}
+        placeholder={props.placeholder}
+        // value={password}
+        // onChange={(e) => setPassword(e.target.value)}
+      />
+    </>
+  );
+};
+
+interface MyButtonProps {
+  type: "button" | "reset" | "submit";
+  title: string;
+}
+
+const MyButton: React.FC<MyButtonProps> = (props) => {
+  return (
+    <>
+      <button
+        type={props.type}
+        className="py-2 px-5 bg-green-400 font-semibold capitalize hover:bg-green-600 duration-200 cursor-pointer hover:text-slate-50 rounded-md shadow-2xl"
+      >
+        {props.title}
+      </button>
+    </>
+  );
+};
